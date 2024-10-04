78f52c9bb7500bebf2e090a2ae4567864ea55a06
Tuan Anh <tp.anhh7@gmail.com>
2024-10-04 22:32:23 +0300

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+	return (
+		arr1.length === arr2.length &&
+		arr1.every((element, i) => element === arr2[i])
+	);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users
+		.filter(userGender => userGender.gender === gender)
+		.map(userAge => userAge.age)
+		.reduce((acc, item, i, arr) => {
+			acc += item;
+			if (i === arr.length - 1) {
+				return acc / arr.length;
+			}
+			return acc;
+		}, 0);
+	return result;
 }
