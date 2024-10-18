80a6613cf08a3989392699571e4f29f9d7343384
Tuan Anh <tp.anhh7@gmail.com>
2024-10-18 11:26:04 +0300

    Initial commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(number) {
+	const parseNumber = Number.parseFloat(number);
+	if (isNan(parseNumber)) {
+		throw new Error('Невалидное значение');
+	}
+	return parseNumber;
+}
+
+function validateCount(number) {
+	try {
+		return parseCount(number);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if (a + b <= c || a + c <= b || b + c <= a) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		const p = this.perimeter / 2;
+		const result = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return Number(result.toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			},
+		};
+	}
+}
